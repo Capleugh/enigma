913a51fe2b83f9fbba3721ed80c93a449f63253d
Carleigh Crockett <capleugh@gmail.com>
2019-11-02 17:30:43 -0600

    Stub generate_random_number in test

--- lib/key.rb
+++ lib/key.rb
@@ -1,6 +1,12 @@
 class Key
+  attr_reader :random
 
   def self.generate_random_number
-    rand(00000..99999).to_s.rjust(5, "0")
+    rand(00000..99999).to_s.rjust(5, '0')
+    # require "pry"; binding.pry
+  end
+
+  def initialize(random)
+    @random = random
   end
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -1,9 +1,12 @@
 require './test_helper'
 require './lib/key'
+require './lib/random'
+require 'mocha/minitest'
 
 class KeyTest < Minitest::Test
   def setup
-    @key = Key.new
+    random = Random.new
+    @key = Key.new(random)
   end
 
   def test_it_exists
@@ -16,5 +19,17 @@ class KeyTest < Minitest::Test
 
   def test_it_is_a_string
     assert_instance_of String, Key.generate_random_number
+  end
+
+  def test_random_number_can_be_mocked
+    Key.expects(:generate_random_number).returns('96287')
+
+    assert_equal '96287', Key.generate_random_number
+  end
+
+  def test_initialize
+    @key.stubs(:random).returns('96287')
+
+    assert_equal '96287', @key.random
   end
 end
