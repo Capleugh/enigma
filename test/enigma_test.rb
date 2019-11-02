b99db73f1a04cf0374c1f7786af146d55792efee
Carleigh Crockett <capleugh@gmail.com>
2019-11-02 14:33:15 -0600

    Create Enigma class and test file

---
+++ test/enigma_test.rb
@@ -0,0 +1,12 @@
+require './test_helper'
+require './lib/enigma'
+
+class EnigmaTest < Minitest::Test
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
+end
