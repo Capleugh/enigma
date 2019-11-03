017b7488976be686fc3486006789d2f132de4e6f
Carleigh Crockett <capleugh@gmail.com>
2019-11-02 20:43:35 -0600

    Implement stubs successfully

--- lib/key.rb
+++ lib/key.rb
@@ -1,20 +1,15 @@
 class Key
   attr_reader :random
 
-  def self.generate_random_number
-    rand(00000..99999).to_s.rjust(5, '0')
-    # require "pry"; binding.pry
-  end
-
   def initialize(random)
     @random = random
-    # require "pry"; binding.pry
   end
 
   def generate_key
-    # require "pry"; binding.pry
+
+    # call the attr_reader instead of the instance variable because stubbing only works for method calls not instance variables, duh carl.
     split = []
-    @random.chars.each_cons(2) do |number|
+    random.chars.each_cons(2) do |number|
       split << number[0].concat(number[1])
     end
     split
--- test/key_test.rb
+++ test/key_test.rb
@@ -1,54 +1,33 @@
 require './test_helper'
 require './lib/key'
+require './lib/random_number'
 require 'mocha/minitest'
 
 class KeyTest < Minitest::Test
   def setup
-    random = Key.generate_random_number
+    random = RandomNumber.generate_random_number
     @key = Key.new(random)
-    @key_2 = Key.new('21456')
+    # @key_2 = Key.new('21456')
   end
 
   def test_it_exists
     assert_instance_of Key, @key
   end
 
-  def test_random_number_length
-    assert_equal 5, Key.generate_random_number.length
-  end
-
-  def test_it_is_a_string
-    assert_instance_of String, Key.generate_random_number
-  end
-
-  def test_random_number_can_be_mocked
-    Key.expects(:generate_random_number).returns('96287')
-
-    assert_equal '96287', Key.generate_random_number
-  end
-
   def test_initialize
-    @key.expects(:random).returns('96287')
-
+    @key.stubs(:random).returns('96287')
     assert_equal '96287', @key.random
-    assert_equal '21456', @key_2.random
   end
-
-
-
-  ##### come back and figure out why this isn't working #####
-
 
   def test_it_can_generate_key
-    expected = ['96', '62', '28', '87']
-    @key.expects(:generate_key).returns(expected)
+    @key.stubs(:random).returns('96287')
 
-    assert_equal expected, @key.generate_key
-    assert_equal ['21', '14', '45', '56'], @key_2.generate_key
+    assert_equal ['96', '62', '28', '87'], @key.generate_key
   end
 
   def test_it_can_convert_strings_to_integers
-    # mock and stub this
-    assert_equal [21, 14, 45, 56], @key_2.convert_key
+    @key.stubs(:generate_key).returns(['96', '62', '28', '87'])
+
+    assert_equal [96, 62, 28, 87], @key.convert_key
   end
 end
