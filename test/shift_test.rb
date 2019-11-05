863dbc02b7853b28d043d30cebdf2365832b4aec
Carleigh Crockett <capleugh@gmail.com>
2019-11-04 17:02:31 -0700

    Change class name from shiftable to shift

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -1,15 +1,15 @@
 require './test_helper'
+require 'mocha/minitest'
 require './lib/random_number'
 require './lib/key'
 require './lib/offset'
 require './lib/shift'
 
-class ShiftableTest < Minitest::Test
+class ShiftTest < Minitest::Test
   def setup
     date = Time.new.strftime('%d%m%y')
     @offset = Offset.new(date)
     @offset.stubs(:date).returns('021119')
-
     RandomNumber.stubs(:generate_random_number).returns('96287')
     @random = RandomNumber.generate_random_number
     @key = Key.new(@random)
