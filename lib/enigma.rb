66bf8d3dfb5b22bf6da360a9eee7d2d9d9bc8bd5
Carleigh Crockett <capleugh@gmail.com>
2019-11-05 18:28:23 -0700

    Add decrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -6,10 +6,17 @@ class Enigma
   end
 
   def encrypt(message, key, date)
-
     encrypted_message =  Encryptor.new(message, key, date).get_encrypted_message(charset)
 
     {encryption: encrypted_message,
+      key: key.random,
+      date: date.date}
+  end
+
+  def decrypt(message, key, date)
+    decrypted_message = Decryptor.new(message, key, date).get_decrypted_message(charset)
+
+    {decryption: decrypted_message,
       key: key.random,
       date: date.date}
   end
